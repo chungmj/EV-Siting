2163b4b0cb68496555dcec5b5e6352b2e57a8332
blevi2ja <80288851+blevi2ja@users.noreply.github.com>
2022-04-25 14:10:15 -0400

    csv files for filtered data

--- Data/FilteringData.py
+++ Data/FilteringData.py
@@ -20,12 +20,21 @@ df81 = df81.sort_values('Latitude')
 df81lvl1 = df81[df81['EV Level1 EVSE Num'] >= 1]
 df81lvl1 = df81lvl1[['State', 'Longitude', 'Latitude', 'EV Level1 EVSE Num']]
 
+#Creating a csv file for the level1 chargers located along I-81
+df81lvl1.to_csv('I-81_Level1_Chargers.csv', index=False)
+
 # The df81lvl2 dataframe filters the data inside the stretch of I-81 to include only the level 2 chargers and their locations
 
 df81lvl2 = df81[df81['EV Level2 EVSE Num'] >= 1]
 df81lvl2 = df81lvl2[['State', 'Longitude', 'Latitude', 'EV Level2 EVSE Num']]
 
+#Creating a csv file for the level2 chargers located along I-81
+df81lvl2.to_csv('I-81_Level2_Chargers.csv', index=False)
+
 # The df81DC dataframe filters the data inside the stretch of I-81 to include only the DC fast chargers and their locations
 
 df81DC = df81[df81['EV DC Fast Count'] >= 1]
 df81DC = df81DC[['State', 'Longitude', 'Latitude', 'EV DC Fast Count']]
+
+#Creating a csv file for the DC Fast chargers located along I-81
+df81DC.to_csv('I-81_DC_Fast_Chargers.csv', index=False)
